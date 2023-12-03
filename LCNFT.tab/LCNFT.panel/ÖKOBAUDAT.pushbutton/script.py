5e6c87dca8c6fc479d5781810113794d2b240f8d
ErichDomme <erich.domme@rwth-aachen.de>
2023-12-03 13:21:05 +0100

    Update script.py

--- LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
+++ LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
@@ -1,8 +1,6 @@
 import clr
 import xml.etree.ElementTree as ET
 clr.AddReference('System.Net')
-clr.AddReference('mscorlib')  # Adding reference to mscorlib which contains System namespace
-from System import Text  # Importing Text from System namespace
 from System.Net import WebClient
 
 def preprocess_xml_data(data):
@@ -11,7 +9,7 @@ def preprocess_xml_data(data):
 
 def get_api_data(url):
     client = WebClient()
-    client.Encoding = Text.Encoding.UTF8  # Set UTF-8 encoding directly
+    client.Encoding = System.Text.Encoding.UTF8
     try:
         data = client.DownloadString(url)
         # Preprocess the data to handle special characters
@@ -25,25 +23,17 @@ def parse_xml_and_print(data):
     try:
         root = ET.fromstring(data)
         namespaces = {
-            'sapi': 'http://www.ilcd-network.org/ILCD/ServiceAPI',  # Confirm this is correct
+            'ns0': 'http://www.ilcd-network.org/ILCD/ServiceAPI/Process',  # Adjusted namespace
+            'ns3': 'http://www.ilcd-network.org/ILCD/ServiceAPI'  # Adjusted namespace
         }
 
-        # Debugging: print root element tag
-        print("Root element:", root.tag)
-
-        # Print first few child elements to understand structure
-        for child in list(root)[:3]:  # Adjust the number to print more or fewer elements
-            print(ET.tostring(child))
-
-        # Adjusted XPath query
-        first_material = root.find('.//sapi:process', namespaces)
+        # Find the first material process
+        first_material = root.find('ns0:process', namespaces)
 
         if first_material is not None:
-            print("First material tag:", first_material.tag)
-
-            uuid = first_material.find('.//sapi:uuid', namespaces).text
-            name = first_material.find('.//sapi:name[@xml:lang="en"]', namespaces).text
-            class_id = first_material.find('.//sapi:class[@level="0"]', namespaces).get('classId')
+            uuid = first_material.find('ns3:uuid', namespaces).text
+            name = first_material.find('ns3:name[@xml:lang="en"]', namespaces).text
+            class_id = first_material.find('ns3:classification/ns3:class[@level="0"]', namespaces).get('classId')
 
             print("UUID: {0}, Name: {1}, Class ID at level 0: {2}".format(uuid, name, class_id))
         else:
@@ -51,10 +41,6 @@ def parse_xml_and_print(data):
 
     except Exception as e:
         print("Error parsing XML: {0}".format(str(e)))
-
-# The rest of the script remains unchanged
-
-
 
 def main():
     url = 'https://oekobaudat.de/OEKOBAU.DAT/resource/datastocks/cd2bda71-760b-4fcc-8a0b-3877c10000a8/processes'
