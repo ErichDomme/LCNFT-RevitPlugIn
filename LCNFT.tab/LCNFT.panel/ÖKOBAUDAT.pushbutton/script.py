e77ece5c1eeea4b97535d79f92a04fea9cc3cd87
ErichDomme <erich.domme@rwth-aachen.de>
2023-12-03 11:58:22 +0100

    Update script.py

--- LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
+++ LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
@@ -11,15 +11,17 @@ try:
     data = client.DownloadString('https://oekobaudat.de/OEKOBAU.DAT/resource/datastocks/cd2bda71-760b-4fcc-8a0b-3877c10000a8/processes')
     
     # Save raw XML content to a file for inspection (if necessary)
-    with open('raw_xml_output.xml', 'w') as file:
+    # Make sure to specify the correct path where the script has write permissions
+    with open('C:\\path\\to\\your\\directory\\raw_xml_output.xml', 'w') as file:
         file.write(data)
     
     # Parse the XML response
     root = ET.fromstring(data)
 
-    # Iterate over the elements and extract data
+    # Now you can iterate over the elements and extract data
     for process in root.findall('{http://www.ilcd-network.org/ILCD/ServiceAPI}process'):
         # Extract details here
+        # Make sure to replace this comment with actual parsing code
 
 except ET.ParseError as pe:
     print("Failed to parse XML: {0}".format(pe))
