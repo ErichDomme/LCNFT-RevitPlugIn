0ffe6e8bf8a9921ba39957a5c2b05cade65b44f2
ErichDomme <erich.domme@rwth-aachen.de>
2023-12-03 15:07:12 +0100

    Update script.py

--- LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
+++ LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
@@ -6,8 +6,10 @@ from System.Text import Encoding
 from System.Net import WebClient
 
 def preprocess_xml_data(data):
-    # Replace  with a suitable replacement or remove it
-    return data.replace(u"\u2122", "")  # Removing  symbol
+    # Replace "trademark" and "registered" with suitable replacements or remove them
+    data = data.replace(u"\u2122", "")  # Removing "trademark" symbol
+    data = data.replace(u"\u00AE", "")  # Removing "registered" symbol
+    return data
 
 def get_api_data(url):
     client = WebClient()
