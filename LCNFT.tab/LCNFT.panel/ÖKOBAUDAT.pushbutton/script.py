ae2b0e42b857c7c5e550b18fc0ed93091fbb3c33
ErichDomme <erich.domme@rwth-aachen.de>
2023-12-03 13:17:57 +0100

    Update script.py

--- LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
+++ LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
@@ -25,22 +25,24 @@ def parse_xml_and_print(data):
     try:
         root = ET.fromstring(data)
         namespaces = {
-            'sapi': 'http://www.ilcd-network.org/ILCD/ServiceAPI',  # Make sure this is correct
+            'sapi': 'http://www.ilcd-network.org/ILCD/ServiceAPI',  # Confirm this is correct
         }
 
         # Debugging: print root element tag
         print("Root element:", root.tag)
 
-        # Find the first material process
+        # Print first few child elements to understand structure
+        for child in list(root)[:3]:  # Adjust the number to print more or fewer elements
+            print(ET.tostring(child))
+
         # Adjusted XPath query
         first_material = root.find('.//sapi:process', namespaces)
 
         if first_material is not None:
-            # More debugging: print first_material tag
             print("First material tag:", first_material.tag)
 
             uuid = first_material.find('.//sapi:uuid', namespaces).text
-            name = first_material.find('.//sapi:name[@xml:lang="en"]', namespaces).text  # Assuming you want the name in English
+            name = first_material.find('.//sapi:name[@xml:lang="en"]', namespaces).text
             class_id = first_material.find('.//sapi:class[@level="0"]', namespaces).get('classId')
 
             print("UUID: {0}, Name: {1}, Class ID at level 0: {2}".format(uuid, name, class_id))
@@ -49,6 +51,9 @@ def parse_xml_and_print(data):
 
     except Exception as e:
         print("Error parsing XML: {0}".format(str(e)))
+
+# The rest of the script remains unchanged
+
 
 
 def main():
