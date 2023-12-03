21eda7d302a31d4aad280a79d16380e56c147664
ErichDomme <erich.domme@rwth-aachen.de>
2023-12-03 12:38:47 +0100

    Update script.py

--- LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
+++ LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
@@ -1,24 +1,47 @@
+import clr
 import xml.etree.ElementTree as ET
+clr.AddReference('System.Net')
+from System.Net import WebClient
 
-# Sample XML string (replace this with the actual XML response)
-xml_data = '''<sapi:dataSetList xmlns:sapi="http://www.ilcd-network.org/ILCD/ServiceAPI">...</sapi:dataSetList>'''
+# Function to get the XML data from the API
+def get_api_data(url):
+    client = WebClient()
+    try:
+        data = client.DownloadString(url)
+        return data
+    except Exception as e:
+        print("Error fetching data: {0}".format(str(e)))
+        return None
 
-# Parse the XML
-root = ET.fromstring(xml_data)
+# Function to parse the XML and extract the desired information
+def parse_xml_and_print(data):
+    try:
+        root = ET.fromstring(data)
+        namespaces = {
+            'sapi': 'http://www.ilcd-network.org/ILCD/ServiceAPI',  # add other namespaces if needed
+        }
 
-# Define your namespaces
-namespaces = {
-    'sapi': 'http://www.ilcd-network.org/ILCD/ServiceAPI',
-    # Add other namespaces as needed
-}
+        # Find the first material process
+        first_material = root.find('sapi:process', namespaces)
+        if first_material is not None:
+            uuid = first_material.find('sapi:uuid', namespaces).text
+            name = first_material.find('sapi:name', namespaces).text
+            class_id = first_material.find('sapi:classification/sapi:class[@level="0"]', namespaces).get('classId')
 
-# Iterate through each dataset
-for dataset in root.findall('sapi:process', namespaces):
-    # Extract the necessary information
-    uuid = dataset.find('sapi:uuid', namespaces).text
-    names = dataset.findall('sapi:name', namespaces)
-    for name in names:
-        lang = name.attrib['{http://www.w3.org/XML/1998/namespace}lang']
-        value = name.text
-        print(f"Name ({lang}): {value}")
-    # Extract other attributes as needed
+            # Print the extracted information
+            print("UUID: {0}, Name: {1}, Class ID at level 0: {2}".format(uuid, name, class_id))
+        else:
+            print("No material found")
+
+    except Exception as e:
+        print("Error parsing XML: {0}".format(str(e)))
+
+# Main function to execute
+def main():
+    url = 'https://oekobaudat.de/OEKOBAU.DAT/resource/datastocks/cd2bda71-760b-4fcc-8a0b-3877c10000a8/processes'
+    xml_data = get_api_data(url)
+    if xml_data:
+        parse_xml_and_print(xml_data)
+
+# Run the main function
+main()
