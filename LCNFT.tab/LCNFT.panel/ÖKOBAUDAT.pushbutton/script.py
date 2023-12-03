bb65a5481cd9a17e7e95418bbd5be53cc1b88c2d
ErichDomme <erich.domme@rwth-aachen.de>
2023-12-03 16:42:29 +0100

    Update script.py

--- LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
+++ LCNFT.tab/LCNFT.panel/ÖKOBAUDAT.pushbutton/script.py
@@ -1,12 +1,14 @@
 import clr
 import xml.etree.ElementTree as ET
 clr.AddReference('System')
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
 clr.AddReference('System.Net')
 from System.Text import Encoding
 from System.Net import WebClient
+from System.Windows.Forms import Application, Form, CheckedListBox, Button, DockStyle
 
 def preprocess_xml_data(data):
-    # Replace "trademark" and "registered" with suitable replacements or remove them
     data = data.replace(u"\u2122", "")  # Removing "trademark" symbol
     data = data.replace(u"\u00AE", "")  # Removing "registered" symbol
     return data
@@ -16,14 +18,13 @@ def get_api_data(url):
     client.Encoding = Encoding.UTF8
     try:
         data = client.DownloadString(url)
-        # Preprocess the data to handle special characters
-        processed_data = preprocess_xml_data(data)
-        return processed_data
+        return preprocess_xml_data(data)
     except Exception as e:
         print("Error fetching data: {0}".format(str(e)))
         return None
 
-def parse_xml_and_print(data):
+def parse_materials(data):
+    materialsByClass = {}
     try:
         root = ET.fromstring(data)
         namespaces = {
@@ -31,28 +32,61 @@ def parse_xml_and_print(data):
             'ns3': 'http://www.ilcd-network.org/ILCD/ServiceAPI'
         }
 
-        # Loop over all material processes
         for material in root.findall('ns0:process', namespaces):
             uuid_elem = material.find('ns3:uuid', namespaces)
             uuid = uuid_elem.text if uuid_elem is not None else "UUID not found"
 
-            # Extract the first <name> element
             name_elem = material.find('ns3:name', namespaces)
             name = name_elem.text if name_elem is not None else "Name not found"
 
             class_elem = material.find('ns3:classification/ns3:class[@level="0"]', namespaces)
             class_name = class_elem.text if class_elem is not None else "Class name not found"
 
-            print("UUID: {0}\nName: {1}\nClass: {2}\n".format(uuid, name, class_name))
-
+            if class_name not in materialsByClass:
+                materialsByClass[class_name] = []
+            materialsByClass[class_name].append({"name": name, "uuid": uuid})
     except Exception as e:
         print("Error parsing XML: {0}".format(str(e)))
+
+    return materialsByClass
+
+class MaterialSelectionForm(Form):
+    def __init__(self, materialsByClass):
+        self.Text = "Select Materials"
+        self.Width = 300
+        self.Height = 400
+
+        self.checkedListBox = CheckedListBox()
+        self.checkedListBox.Dock = DockStyle.Fill
+        self.checkedListBox.CheckOnClick = True
+
+        self.populate_materials(materialsByClass)
+
+        self.okButton = Button()
+        self.okButton.Text = 'OK'
+        self.okButton.Dock = DockStyle.Bottom
+        self.okButton.Click += self.button_clicked
+
+        self.Controls.Add(self.checkedListBox)
+        self.Controls.Add(self.okButton)
+
+    def populate_materials(self, materialsByClass):
+        for className, materials in materialsByClass.items():
+            for material in materials:
+                item = "{0} - {1} ({2})".format(className, material['name'], material['uuid'])
+                self.checkedListBox.Items.Add(item)
+
+    def button_clicked(self, sender, args):
+        selected_materials = [self.checkedListBox.Items[i] for i in range(self.checkedListBox.Items.Count) if self.checkedListBox.GetItemChecked(i)]
+        print(selected_materials)  # For testing purposes
+        self.Close()
 
 def main():
     url = 'https://oekobaudat.de/OEKOBAU.DAT/resource/datastocks/cd2bda71-760b-4fcc-8a0b-3877c10000a8/processes'
     xml_data = get_api_data(url)
     if xml_data:
-        parse_xml_and_print(xml_data)
+        materialsByClass = parse_materials(xml_data)
+        form = MaterialSelectionForm(materialsByClass)
+        Application.Run(form)
 
-# Run the main function
 main()
